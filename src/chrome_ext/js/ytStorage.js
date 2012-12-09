610e19c5c3b67a5e77909fcb359764d7340d3baf
Paul Comanici <darkyndy@gmail.com>
2012-12-09 19:57:35 +0200

    - storage abstraction;

---
+++ src/chrome_ext/js/ytStorage.js
@@ -0,0 +1,82 @@
+/**
+ * @description Storage abstraction
+ */
+var YtStorage = (function () {
+	var my,
+		myStorageSync,
+		myLocalStorage;
+	/**
+	 * @description Local storage
+	 */
+	myLocalStorage = {
+		/**
+		 * @description Get storage item
+		 *   if item doesn't exist then set it to itemValue
+		 * @param {String} itemName
+		 * @param {Function} cbAtGet
+		 */
+		getItem: function getItem(itemName, cbAtGet) {
+			var proprVal;
+			proprVal = localStorage[itemName];
+			if (cbAtGet) {
+				cbAtGet(itemName, proprVal);
+			}
+		},
+		/**
+		 * 
+		 * @description Set storage item
+		 * @param {String} itemName
+		 * @param {String} itemValue
+		 * @param {Function} cbAtSet
+		 */
+		setItem: function setItem(itemName, itemValue, cbAtSet) {
+			localStorage.setItem(itemName, itemValue);
+			if (cbAtSet) {
+				cbAtSet(itemName, itemValue);
+			}
+		}
+	};
+	/**
+	 * @description Chrome storage sync
+	 */
+	myStorageSync = {
+			/**
+			 * @description Get storage item
+			 *   if item doesn't exist then set it to itemValue
+			 * @param {String} itemName
+			 * @param {String} itemValue
+			 */
+			getItem: function getItem(itemName, itemValue, cbAtGet) {
+				chrome.storage.sync.get(itemName, function (item) {
+					if (cbAtGet) {
+						cbAtGet(itemName, item[itemName]);
+					}
+				});
+			},
+			/**
+			 * @description Set storage item
+			 * @param {String} itemName
+			 * @param {String} itemValue
+			 */
+			setItem: function setItem(itemName, itemValue) {
+				chrome.storage.sync.set({itemName: itemValue});
+			}
+	};
+	/**
+	 * @description Initialize
+	 */
+	function init() {
+		if (chrome && chrome.storage && chrome.storage.sync) {
+			//useStorage = myStorageSync;
+		} else {
+			useStorage = localStorage;
+		}
+		useStorage = localStorage;
+	}
+	init();
+	//return public methods
+	return {
+		getItem: useStorage.getItem,
+		setItem: useStorage.setItem
+	};
+}());
