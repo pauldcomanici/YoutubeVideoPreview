b5321c0e29273c49fe731b3a9396cc43312fb9ae
Paul Comanici <darkyndy@gmail.com>
2012-12-10 23:38:04 +0200

    - i18n for popup.html

--- src/chrome_ext/js/ytTranslate.js
+++ src/chrome_ext/js/ytTranslate.js
@@ -5,16 +5,50 @@
 (function () {
 	"use strict";
 	/*global chrome, document*/
+	/**
+	 * @description Add localization
+	 *     find element with id -> elementId
+	 *     and set for it i18n string found under messageKey
+	 *     this set is done based on setAs parameter that can be "textContent" or "value"
+	 *       by default is "textContent"
+	 * @param {String} elementId
+	 * @param {String} messageKey
+	 * @param {String} setAs
+	 */
+	function addLocalization(elementId, messageKey, setAs) {
+		var el;
+		el = document.getElementById(elementId);
+		if (el) {
+			setAs = setAs || "textContent";
+			el[setAs] = chrome.i18n.getMessage(messageKey);
+		}
+	}
+	/**
+	 * @description Execute translation
+	 */
 	function execute() {
-		document.getElementById("headerTitle").textContent = chrome.i18n.getMessage("extensionName");
-		document.getElementById("page_feedback").textContent = chrome.i18n.getMessage("feedback");
-		document.getElementById("page_about").textContent = chrome.i18n.getMessage("about");
-		document.getElementById("page_options").textContent = chrome.i18n.getMessage("options");
-		document.getElementById("imageTimeLabel").textContent = chrome.i18n.getMessage("imageTimeLabel");
-		document.getElementById("imageTimeUnit").textContent = chrome.i18n.getMessage("imageTimeDescription");
-		document.getElementById("enableRatingViewLabel").textContent = chrome.i18n.getMessage("enableRatingViewLabel");
-		document.getElementById("enableRatingViewTrue").textContent = chrome.i18n.getMessage("enableRatingViewTrue");
-		document.getElementById("enableRatingViewFalse").textContent = chrome.i18n.getMessage("enableRatingViewFalse");
+		//general strings
+		addLocalization("loading", "loading");
+		//site page
+		addLocalization("headerTitle", "extensionName");
+		addLocalization("page_feedback", "feedback");
+		addLocalization("page_about", "about");
+		addLocalization("page_options", "options");
+		addLocalization("imageTimeLabel", "imageTimeLabel");
+		addLocalization("imageTimeUnit", "imageTimeDescription");
+		addLocalization("enableRatingViewLabel", "enableRatingViewLabel");
+		addLocalization("enableRatingViewTrue", "enableRatingViewTrue");
+		addLocalization("enableRatingViewFalse", "enableRatingViewFalse");
+		//popup page
+		addLocalization("ytPreviewOptions", "popupOptions");
+		addLocalization("ytPreviewHideIcon", "popupHideIcon");
+		addLocalization("popupAboutHideIcon", "popupAboutHideIcon");
+		addLocalization("areYouSureQuestion", "popupAreYouSureQuestion");
+		addLocalization("hideIconOptionsYes", "yes", "value");
+		addLocalization("hideIconOptionsNo", "no", "value");
+		addLocalization("hideIconConfirmLabel", "popupHideIconConfirmLabel");
+		addLocalization("hideIconOptionsInfoDesc", "popupHideIconOptionsInfoDesc");
+		addLocalization("hideIconOptionsInfoPath", "popupHideIconOptionsInfoPath");
 	}
 	//execute translation
 	execute();
