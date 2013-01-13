c42274177cf533bd2f6635c676477ea59af4c64e
Paul Comanici (darkyndy) <darkyndy@gmail.com>
2013-01-13 22:50:15 +0200

    - FireFox extension prepare settings

--- src/firefox_ext/lib/main.js
+++ src/firefox_ext/lib/main.js
@@ -1,19 +1,44 @@
+/**
+ * @description Extension background logic
+ */
+/*jslint browser: true, devel: true */
 var YtpBackground = (function () {
+    "use strict";
+    /*global require*/
     var my,
-        publicMethods;
+        publicMethods,
+        settingsPanel;
     my = {
+        /**
+         * @description Modules used by extension
+         */
         mods: {
             "page": (function () {
                 return require("page-mod");
             }()),
             "self": (function () {
                 return require("self");
+            }()),
+            "panel": (function () {
+                return require("panel");
+            }()),
+            "storage": (function () {
+                return require("simple-storage");
+            }()),
+            "prefs": (function () {
+                return require("simple-prefs");
             }())
         },
+        /**
+         * @description Attach events
+         */
         delegate: function attachEvents() {
         },
+        /**
+         * @description Worker for page module
+         */
         pageModWorker: function pageModWorker(worker) {
-            worker.on('message', function(request) {
+            worker.on('message', function (request) {
                 var reqMsg,
                     reqCb,
                     responseObj;
@@ -44,6 +69,9 @@ var YtpBackground = (function () {
                 }
             });
         },
+        /**
+         * @description Activate pageMod
+         */
         setPageMod: function () {
             var data;
             data = my.mods.self.data;
@@ -55,12 +83,34 @@ var YtpBackground = (function () {
                 onAttach: my.pageModWorker
             });
         },
+        /**
+         * @description Activate settings panel
+         */
+        setSettingsPanel: function () {
+            settingsPanel = my.mods.panel.Panel({
+                width: 300,
+                height: 200,
+                contentURL: my.mods.self.data.url("settings.html")
+            });
+        },
+        /**
+         * @description Activate preferences
+         */
+        setPreferences: function () {
+            my.mods.prefs.on("ytvpSettings", function () { settingsPanel.show(); });
+        },
+        /**
+         * @description Initialize extension
+         */
         init: function () {
             my.delegate();
             my.setPageMod();
+            my.setSettingsPanel();
+            my.setPreferences();
         }
     };
     my.init();
+    //public methods
     publicMethods = {
         mods: my.mods
     };
